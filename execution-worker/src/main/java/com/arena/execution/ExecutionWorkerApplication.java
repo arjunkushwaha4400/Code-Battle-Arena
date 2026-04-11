06ed02ab5d2bfe849bcf4a446ce20b4befbaec50
Arjun Kushwaha <arjun.kushwaha20234037@gmail.com>
2026-04-11 10:46:55 +0100

    add eureka server

--- execution-worker/src/main/java/com/arena/execution/ExecutionWorkerApplication.java
+++ execution-worker/src/main/java/com/arena/execution/ExecutionWorkerApplication.java
@@ -2,8 +2,10 @@ package com.arena.execution;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.arena.execution", "com.arena.common"})
+@EnableDiscoveryClient
 public class ExecutionWorkerApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/arena/user/UserServiceApplication.java
+++ user-service/src/main/java/com/arena/user/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.arena.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.arena.user", "com.arena.common"})
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
